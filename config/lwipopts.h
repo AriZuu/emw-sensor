ca1bbf22bf03cdea1966d45c0a83381fd51eb27d
Ari Suutari <ari@stonepile.fi>
2017-01-10 21:15:42 +0200

    Minor tuning for lwip 2.0.1.

--- config/lwipopts.h
+++ config/lwipopts.h
@@ -85,8 +85,6 @@
 #define TCP_SND_QUEUELEN                6
 #define MEMP_NUM_TCP_SEG                16
 
-int sys_random(void);
-#define LWIP_RAND() sys_random()
 #define LWIP_COMPAT_SOCKETS		0
 
 void setSystemTime(time_t);
